328c7d1ce0af7841a4f65a847e6b59df7b2d2bbb
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-11-02 22:10:17 +0100

    add movie instances

--- classes/Movie.php
+++ classes/Movie.php
@@ -2,29 +2,84 @@
 
 class Movie{
 
-    public $title;
-    public $year;
-    public $director;
-    public $language;
-    public $rating;
+    // public $title;
+    // public $year;
+    // public $director;
+    // public $language;
+    // public $rating;
+    private $infos;
 
-    public function __construct($_title, $_year, $_director, $_language, $_rating)
+    public function __construct($_infos)
     {
-        $this->title = $_title;
-        $this->year = $_year;
-        $this->director = $_director;
-        $this->language = $_language;
-        $this->rating = $_rating;
+        $this-> infos['title']= $_infos["title"];
+        $this-> infos['year']= $_infos["year"];
+        $this-> infos['director']= $_infos["director"];
+        $this-> infos['language']= $_infos["language"];
+        $this-> infos['rating']= $_infos["rating"];
+
+        // $this->title = $_title;
+        // $this->year = $_year;
+        // $this->director = $_director;
+        // $this->language = $_language;
+        // $this->rating = $_rating;
     }
 
-    public function setTitle($_title) {
-        if ( is_string($_title) ) {
-            $this->title = $_title;
+
+    // SET
+    public function setTitle($_infos) {
+        if ( is_string($_infos["title"]) ) {
+            $this->infos["title"] = $_infos["title"];
         }
     }
-    public function getTitle() {
-        return $this->title;
+
+    public function setYear($_infos) {
+        if ( is_numeric($_infos["year"]) ) {
+            $this->infos["year"] = $_infos["year"];
+        }
     }
+
+    public function setDirector($_infos) {
+        if ( is_string($_infos["director"]) ) {
+            $this->infos["director"] = $_infos["director"];
+        }
+    }
+
+    public function setLanguage($_infos) {
+        if ( is_string($_infos["language"]) ) {
+            $this->infos["language"] = $_infos["language"];
+        }
+    }
+
+    public function setRating($_infos) {
+        if ( is_numeric($_infos["rating"]) ) {
+            $this->infos["rating"] = $_infos["rating"];
+        }
+    }
+
+
+
+
+    // GET
+    function getInfo($value) {
+        return $this->infos[$value];
+    }
+
+    // function getYear(){
+    //     return $this->infos["year"];
+    // }
+
+    // function getDirector(){
+    //     return $this->infos["director"];
+    // }
+
+    // function getLanguage(){
+    //     return $this->infos["language"];
+    // }
+
+    // function getRating(){
+    //     return $this->infos["rating"];
+    // }
+   
     
 
 }
--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 require_once __DIR__ . '/classes/Movie.php';
 
-$backToFuture = new Movie('Back to future: part I', 1985, 'Robert Zemeckis', 'ENG', 5);
+$movies= [
+    // $backToFuture = new Movie('Back to future: part I', 1985, 'Robert Zemeckis', 'ENG', 5);
+    new Movie(
+        [
+            "title"=>'Back to future: part I',
+            "year"=>1985,
+            "director"=>'Robert Zemeckis',
+            "language"=>'eng',
+            "rating"=>5
+        ]
+    ),
 
-// var_dump($backToFuture);
-// echo $backToFuture;
-
-
+    new Movie(
+        [
+            "title"=>'Interstellar',
+            "year"=>2014,
+            "director"=>'Christopher Nolan',
+            "language"=>'eng',
+            "rating"=>5
+        ]
+    )
+]
 
 ?>
 
@@ -20,7 +36,29 @@ $backToFuture = new Movie('Back to future: part I', 1985, 'Robert Zemeckis', 'EN
     </head>
     <body>
 
-        <?php echo $backToFuture->getTitle() ?>
+      <ul>
+      
+        <?php foreach($movies as $movie) :  ?>
+            <li>
+                <h2><?= $movie -> getInfo('title')?> </h2>
+            </li>
+            <li>
+                <?= $movie -> getInfo('year')?> 
+            </li>
+            <li>
+                <?= $movie -> getInfo('director')?> 
+            </li>
+            <li>
+                <?= $movie -> getInfo('language')?> 
+            </li>
+            <li>
+                <?= $movie -> getInfo('rating')?> 
+            </li>
+            
+        <?php endforeach?> 
+      </ul>  
+      
+     
 
     
 
